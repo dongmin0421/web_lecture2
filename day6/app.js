33c92fe4804e303a370010a481bc3355183aee36
dongmin0421 <105410962+dongmin0421@users.noreply.github.com>
2022-05-17 15:23:36 +0900

    event

---
+++ day6/app.js
@@ -0,0 +1,25 @@
+
+const calulator = {
+    name:"계산기",
+    add: function (num1, num2) {
+        return (num1+num2);
+    },
+    minus: function (num1, num2) {
+        return (num1 - num2);
+    },
+    dvide: function (num1, num2) {
+        return (num1 / num2);
+    },
+    multify: function (num1, num2) {
+        return (num1 * num2);
+    },
+    sqard: function (num1, num2) {
+        return (num1 ** num2);
+    },
+}
+const player = {
+    name: "전사",
+    mod: function (num1, num2) {
+        return (num1 % num2);
+    },
+}
